42c41939650f164edf978e1437057c63fd6b4008
AlanPa <alan.paugois@gmail.com>
2018-05-20 18:36:53 +0200

    Version originale du projet

---
+++ src/main/java/action/Action.java
@@ -0,0 +1,157 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package action;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import entities.Client;
+import entities.Employe;
+import entities.Medium;
+import entities.Voyance;
+import java.io.PrintWriter;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import service.Service;
+
+/**
+ *
+ * @author alan
+ */
+public class Action 
+{
+    
+    public static String Inscription(HttpServletRequest request) throws ParseException
+    {       
+            String nom=request.getParameter("nom");
+            String prenom=request.getParameter("prenom");
+            String adresseMail=request.getParameter("mail");
+            String adressePostale=request.getParameter("adresse");
+            String civilite=request.getParameter("civilite");
+            String numeroTel=request.getParameter("tel");
+            try
+            {
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+            String str_date=request.getParameter("date");
+            Date dateNaissance=sdf.parse(str_date);
+            
+            if(Service.obtenirClient(adresseMail) != null)
+            {
+                return("False");
+            }
+            else
+            {
+                System.out.println(str_date);
+                Client cl = new Client(civilite,nom,prenom,dateNaissance,adressePostale,numeroTel,adresseMail);
+                Service.creerClient(cl);
+                 return("True");
+            }
+            }
+            catch( ParseException e)
+            {
+                 System.out.println("Erreur");
+              return "Error" ;
+            }      
+            
+            
+        
+    }
+    
+    public static String Connexion(HttpServletRequest request, HttpSession session){
+        String mail = request.getParameter("mail");
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        JsonObject jsonConnex = new JsonObject();
+        JsonObject container = new JsonObject();
+        String[] tab = mail.split("@");
+        int size = tab.length;
+        if (tab[size-1].equals("posit.if")){
+
+            Employe e = (Employe) Service.obtenirEmploye(mail);
+            if (e != null)
+            {
+                session.setAttribute("nom",e.getNom());  
+                session.setAttribute("prenom",e.getPrenom());
+                jsonConnex.addProperty("type", "Employe");
+                
+                //jsonConnex.addProperty("listeMedium", e.getListMedium());
+                JsonArray listeMed = new JsonArray();
+                 for (Medium m: e.getListMedium()){
+                  JsonObject medium = new JsonObject();
+                  medium.addProperty("date",m.getNom());
+                  listeMed.add(medium);
+                  
+              }
+                jsonConnex.add("listeMed", listeMed);
+                
+                JsonArray historique = new JsonArray();
+                for (Voyance v: e.getListeVoyances()){
+                  JsonObject voyance = new JsonObject();
+                  voyance.addProperty("date",v.getHeureDebut().getTime());
+                  voyance.addProperty("duree", (v.getHeureFin().getTime()-(v.getHeureDebut()).getTime()) );
+                  voyance.addProperty("nom", v.getMedium().getNom());
+                  historique.add(voyance);
+                  
+              }
+                jsonConnex.add("historique", historique);
+                container.add("connex", jsonConnex);
+            }
+            else
+            {
+                jsonConnex.addProperty("type", "error");
+                container.add("connex", jsonConnex);
+            }
+
+        }
+        else{
+            Client c = Service.obtenirClient(mail); 
+            if (c != null)
+            {
+              session.setAttribute("nom",c.getNom());  
+              session.setAttribute("prenom",c.getPrenom());
+              jsonConnex.addProperty("type", "Client");
+              jsonConnex.addProperty("nom",c.getNom());  
+              jsonConnex.addProperty("prenom",c.getPrenom());
+              jsonConnex.addProperty("couleur", c.getCouleur());
+              jsonConnex.addProperty("animal",c.getAnimalTotem());
+              jsonConnex.addProperty("zodiaque",c.getSigneZodiaque());
+              jsonConnex.addProperty("chinois",c.getSigneChinois());
+              
+              JsonArray historique = new JsonArray();
+              for (Voyance v: c.getListVoyances()){
+                  JsonObject voyance = new JsonObject();
+                  voyance.addProperty("date",v.getHeureDebut().getTime());
+                  voyance.addProperty("duree", (v.getHeureFin().getTime()-(v.getHeureDebut()).getTime()) );
+                  voyance.addProperty("nom", v.getMedium().getNom());
+                  historique.add(voyance);
+                  
+              }
+              jsonConnex.add("historique", historique);
+              container.add("connex", jsonConnex);
+            }
+            else
+            {
+              jsonConnex.addProperty("type", "error");
+              jsonConnex.addProperty("nom","error");  
+              jsonConnex.addProperty("prenom","error");
+              jsonConnex.addProperty("couleur", "error");
+              jsonConnex.addProperty("animal","error");
+              jsonConnex.addProperty("zodiaque","error");
+              jsonConnex.addProperty("chinois","error"); 
+              container.add("connex", jsonConnex); 
+            }
+            
+            
+        }
+        System.out.println(container);
+        return(gson.toJson(container));
+}
+    
+    
+}
---
+++ src/main/java/servlet/ActionServlet.java
@@ -0,0 +1,121 @@
+package servlet;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import action.Action;
+import dao.JpaUtil;
+import entities.Client;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.text.ParseException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ *
+ * @author alan
+ */
+@WebServlet(urlPatterns = {"/ActionServlet"})
+public class ActionServlet extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException, ParseException {
+        response.setContentType("text/html;charset=UTF-8");
+        String action=request.getParameter("action");
+        HttpSession session = request.getSession();
+        try (PrintWriter out = response.getWriter()) {
+            if(action.equals("inscription"))
+            {
+                
+                out.print(Action.Inscription(request));
+            }
+            else if(action.equals("connexion"))
+            {
+               out.print(Action.Connexion(request,session));
+            }
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        try {
+            processRequest(request, response);
+        } catch (ParseException ex) {
+            Logger.getLogger(ActionServlet.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        try {
+            processRequest(request, response);
+        } catch (ParseException ex) {
+            Logger.getLogger(ActionServlet.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+    
+    @Override
+    public void init() throws ServletException {
+        super.init();
+        JpaUtil.init();
+    }
+    
+    /**
+     *
+     * @throws ServletException
+     */
+    @Override
+    public void destroy() {
+        super.destroy();
+        JpaUtil.destroy();
+    }
+    
+}
